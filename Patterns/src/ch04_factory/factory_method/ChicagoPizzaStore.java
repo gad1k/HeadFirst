534be0fc3d644906ef9feee04167f556ff936664
Andrei Hryshechkin <gad1k@list.ru>
2022-02-03 00:02:51 +0300

    Factory pattern has been added (Factory method)

---
+++ Patterns/src/ch04_factory/factory_method/ChicagoPizzaStore.java
@@ -0,0 +1,17 @@
+package ch04_factory.factory_method;
+
+public class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    Pizza createPizza(String item) {
+        if (item.equals("cheese"))
+            return new ChicagoStyleCheesePizza();
+        else if (item.equals("pepperoni"))
+            return new ChicagoStylePepperoniPizza();
+        else if (item.equals("clam"))
+            return new ChicagoStyleClamPizza();
+        else if (item.equals("veggie"))
+            return new ChicagoStyleVeggiePizza();
+        else
+            return null;
+    }
+}
