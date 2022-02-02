597200c775792995ebf3fd4c560ffe80c9dbbb30
Andrei Hryshechkin <gad1k@list.ru>
2022-02-02 21:42:57 +0300

    Simple factory pattern has been added

---
+++ Patterns/src/ch04_factory/simple_factory/PizzaTestDrive.java
@@ -0,0 +1,17 @@
+package ch04_factory.simple_factory;
+
+public class PizzaTestDrive {
+    public static void main(String[] args) {
+        SimplePizzaFactory factory = new SimplePizzaFactory();
+        PizzaStore store = new PizzaStore(factory);
+        Pizza pizza;
+
+        pizza = store.orderPizza("cheese");
+        System.out.println("We ordered a " + pizza.getName());
+        System.out.println(pizza);
+
+        pizza = store.orderPizza("veggie");
+        System.out.println("We ordered a " + pizza.getName());
+        System.out.println(pizza);
+    }
+}
